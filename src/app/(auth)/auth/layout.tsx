ae05fa9c805b00bf023f4b03818278a3e4721d96
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-11-25 12:05:37 +0600

    all ok favicon

--- src/app/(auth)/auth/layout.tsx
+++ src/app/(auth)/auth/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 
-
 export const metadata: Metadata = {
-  title: "Auth",
-  description: "Auth",
+  title: "PASS IT ALONG - Auth",
+  description: "Authentication for Pass It Along - Ripple Effect Giving",
+  icons: {
+    icon: "/favicon_white.svg",
+  },
 };
 
 export default function AuthLayout({
@@ -12,7 +14,9 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className={` antialiased bg-black w-full  sm:w-[420px] mx-auto min-h-[100vh] flex items-center justify-center`}>
+    <div
+      className={` antialiased bg-black w-full  sm:w-[420px] mx-auto min-h-[100vh] flex items-center justify-center`}
+    >
       {children}
     </div>
   );
