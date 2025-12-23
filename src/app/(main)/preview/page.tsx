a0b8a7973263e13dce6145cc1e0ccaf40b31f3cd
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-12-23 15:12:15 +0600

    new branch

--- src/app/(main)/preview/page.tsx
+++ src/app/(main)/preview/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Preview from "@/components/main/preview/Preview";
 import AuthGuard from "@/components/common/AuthGuard";
+import Navigation from "@/components/common/backbutton/backbutton";
 
 function page() {
   return (
     <AuthGuard login={true} role={["USER"]} redirectTo="/preview">
       <div className="w-full bg-gray-100 rounded-2xl p-6">
         <Preview />
+        <Navigation />
       </div>
     </AuthGuard>
   );
--- src/app/(main)/your-why/page.tsx
+++ src/app/(main)/your-why/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import YourWhy from "@/components/main/yourwhy/yourWhy";
 import AuthGuard from "@/components/common/AuthGuard";
+import Navigation from "@/components/common/backbutton/backbutton";
 
 function page() {
   return (
     <AuthGuard login={true} role={["USER"]} redirectTo="/">
       <div className="w-full bg-gray-100 rounded-2xl p-6">
         <YourWhy />
+        <Navigation />
       </div>
     </AuthGuard>
   );
