4548b72deef25fd980ece6f426db0cf5975a1642
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-12-31 16:52:38 +0600

    live url

--- src/store/baseUrl.ts
+++ src/store/baseUrl.ts
@@ -1,9 +1,11 @@
 export const getBaseUrl = () => {
-  return "https://ismail4001.binarybards.online/api/v1";
+  // return "https://ismail4001.binarybards.online/api/v1";
+  return "http://204.197.173.144:4001/api/v1";
   // return "http://10.10.7.102:9002/api/v1";
 };
 
 export const ImageUrl = () => {
-  return "https://ismail4001.binarybards.online/";
+  // return "https://ismail4001.binarybards.online/";
+  return "http://204.197.173.144:4001/";
   // return "http://10.10.7.102:9002/";
 };
